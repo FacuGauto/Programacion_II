255510634cac762ffdd9ac311181580263db03e9
alumno <alumno>
2018-08-22 21:07:33 -0300

    primer commit

---
+++ Ejercicio_01/Ejercicio_01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_01
+{
+    class Ejercicio_01
+    {
+        static void Main(string[] args)
+        {
+            int i,num;
+            int maximo = int.MaxValue;
+            int minimo = int.MinValue;
+            int suma = 0;
+            int promedio;
+            string numero;
+
+            Console.Title = "Ejerciocio  Nro 01";
+
+            for(i=0;i<5; i++)
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                numero = Console.ReadLine();
+                num = int.Parse(numero);
+                suma += num;
+                
+                if(maximo<num)
+                {
+                    maximo = num;
+                }
+                if(minimo>num)
+                {
+                    minimo = num;
+                }
+            }
+            promedio = suma/5;
+            Console.WriteLine("Numero Maximo: {0}",maximo);
+            Console.WriteLine("Numero Minimo: {0}",minimo);
+            Console.WriteLine("Promedio: {0}", promedio);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio_01/Ejercicio_02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_02
+{
+    class Ejercicio_02
+    {
+        static void Main(string[] args)
+        {
+            string numero;
+            int num;
+            long cuadrado;
+            long cubo;
+
+            Console.WriteLine("Ingrese un numero: ");
+            numero = Console.ReadLine();
+            num = int.Parse(numero);
+            cuadrado = (long)Math.Pow(num,2);
+            cubo = (long)Math.Pow(num, 3);
+
+            Console.WriteLine("Numero al cuadrado: {0}", cuadrado);
+            Console.WriteLine("Numero al cubo: {0}", cubo);
+            Console.ReadKey();
+        }
+    }
+}
