4012662f3e52c6bd8b2da977fc191d3516cf6e75
alumno <alumno>
2018-08-23 21:06:42 -0300

    Ejercicio 6

--- Ejercicio_01/Ejercicio_06/Program.cs
+++ Ejercicio_01/Ejercicio_06/Program.cs
@@ -10,7 +10,35 @@ namespace Ejercicio_06
     {
         static void Main(string[] args)
         {
+            int año;
+            string añoLetras;
+            Console.WriteLine("Ingrese un año: ");
+            añoLetras = Console.ReadLine();
+            año = int.Parse(añoLetras);
 
+            if((año % 4) ==0)
+            {
+                if ((año % 100) == 0)
+                {
+                    if((año % 400)==0)
+                    {
+                        Console.WriteLine("Año bisiesto: {0}", año);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No es un año bisiesto.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Año bisiesto: {0}", año);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No es un año bisiesto.");
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ Ejercicio_01/Ejercicio_07/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_07
+{
+    class Ejercicio_07
+    {
+        static void Main(string[] args)
+        {
+            int dia,mes,año,cantDiasVivivdos=0;
+            int añoActual,mesActual,diaActual;
+            string diaL, mesL, añoL;
+            DateTime hoy = DateTime.Now;
+            añoActual = hoy.Year;
+            mesActual = hoy.Month;
+            diaActual = hoy.Day;
+
+
+            Console.WriteLine("Ingrese su dia de nacimiento: ");
+            diaL = Console.ReadLine();
+            dia = int.Parse(diaL);
+            Console.WriteLine("Ingrese su mes de nacimiento: ");
+            mesL = Console.ReadLine();
+            mes = int.Parse(mesL);
+            Console.WriteLine("Ingrese su año de nacimiento: ");
+            añoL = Console.ReadLine();
+            año = int.Parse(añoL);
+
+            if(año == añoActual)
+            {
+
+            }
+            for(int i = añp; ;i++)
+            {
+
+            }
+
+        }
+    }
+}
