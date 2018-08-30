8b36b93a0e113eaae8f0a1bf2050ad8f82aef960
alumno <alumno>
2018-08-30 19:44:28 -0300

    Ejercicio 16

---
+++ Ejercicio_01/Ejercicio_16/Alumno.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_16
+{
+    class Alumno
+    {
+        public string nombre;
+        public string apellido;
+        public string legajo;
+        private byte nota1;
+        private byte nota2;
+        private float notaFinal;
+
+        public void calcularFinal()
+        {
+            this.notaFinal = -1;
+            Random objRandom = new Random();
+            if(this.nota1 >= 4 && this.nota2 >= 4)
+            {
+                this.notaFinal = objRandom.Next(5, 10);
+            }
+        }
+
+        public void Estudiar(byte notaUno,byte notaDos)
+        {
+            this.nota1 = notaUno;
+            this.nota2 = notaDos;
+        }
+        public void Mostrar()
+        {
+            Console.WriteLine("Nombre: {0}, apellido: {1}, legajo: {2}, nota 1: {3}, nota 2: {4}", this.nombre, this.apellido, this.legajo, this.nota1, this.nota2);
+            if (this.notaFinal != -1)
+            {
+                Console.WriteLine("Nota Final: {0}.\n",this.notaFinal);
+            }
+            else
+            {
+                Console.WriteLine("Alumno desaprobado.\n");
+            }
+        }
+    }
+}
---
+++ Ejercicio_01/Ejercicio_16/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_16
+{
+    class Ejercicio_16
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumnoUno = new Alumno();
+            Alumno alumnoDos = new Alumno();
+            Alumno alumnoTres = new Alumno();
+
+            alumnoUno.nombre = "Bruno";
+            alumnoUno.apellido = "Lopez";
+            alumnoUno.legajo = "0001";
+            alumnoUno.Estudiar(10, 8);
+            alumnoUno.calcularFinal();
+
+            alumnoDos.nombre = "Julia";
+            alumnoDos.apellido = "Lopez";
+            alumnoDos.legajo = "0002";
+            alumnoDos.Estudiar(7, 3);
+            alumnoDos.calcularFinal();
+
+            alumnoTres.nombre = "Mariano";
+            alumnoTres.apellido = "Gomez";
+            alumnoTres.legajo = "0003";
+            alumnoTres.Estudiar(5, 8);
+            alumnoTres.calcularFinal();
+
+            alumnoUno.Mostrar();
+            alumnoDos.Mostrar();
+            alumnoTres.Mostrar();
+
+            Console.ReadKey();
+        }
+    }
+}
