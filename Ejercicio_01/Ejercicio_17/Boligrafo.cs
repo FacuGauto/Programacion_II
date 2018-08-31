7890b442ab161963a9a094083368943ef8790a64
alumno <alumno>
2018-08-30 21:47:49 -0300

    Ejercicio 17

---
+++ Ejercicio_01/Ejercicio_17/Boligrafo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_17
+{
+    class Boligrafo
+    {
+        const short cantidadTintaMaxima = 100;
+        public ConsoleColor color;
+        private short tinta;
+
+        public ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+        public short GetTinta()
+        {
+            return this.tinta;
+        }
+        public void SetTinta(short tinta)
+        {
+            if (tinta > 0 && (this.tinta + tinta)<cantidadTintaMaxima)
+            {
+                this.tinta += tinta;
+            }
+            if (tinta<0 && (this.tinta - tinta) >= 0)
+            {
+                this.tinta += tinta;
+            }
+        }
+        public void Recargar()
+        {
+            this.tinta = cantidadTintaMaxima;
+        }
+        public Pintar(int tintaAPintar, out String gasto)
+        {
+            if (this.tinta>0 &&tintaAPintar>0)
+            {
+                while(this.tinta>0 && tintaAPintar>0)
+                {
+                    tintaAPintar--;
+                    this.tinta--;
+                    gasto += "*";
+                }
+            }
+        }
+    }
+}
---
+++ Ejercicio_01/Ejercicio_17/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_17
+{
+    class Ejercicio_17
+    {
+        static void Main(string[] args)
+        {
+            Boligrafo boligrafoAzul = new Boligrafo();
+            Boligrafo boligrafoRojo = new Boligrafo();
+            boligrafoAzul.SetTinta(100);
+            boligrafoRojo.SetTinta(50);
+            boligrafoAzul.color = ConsoleColor.Blue;
+            boligrafoRojo.color = ConsoleColor.Red;
+
+        }
+    }
+}
