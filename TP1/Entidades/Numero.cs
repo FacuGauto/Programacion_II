7fd4e7af9f895321dd37d3c62da439a5f815e2bc
FacuGauto <fgauto1992@gmail.com>
2018-09-13 04:56:57 -0300

    tp1

---
+++ TP1/Entidades/Numero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Numero
+    {
+        private double numero;
+
+        private double ValidarNumero(string strNumero)
+        {
+            double numero;
+            if(double.TryParse(strNumero,out numero))
+            {
+                return numero;
+            }
+            return 0;
+        }
+
+    }
+}
