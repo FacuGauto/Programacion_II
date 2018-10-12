3620ede1b3e7db7880c6b273a2055fe4f34e1696
alumno <alumno>
2018-10-11 21:35:11 -0300

    exception

---
+++ Ejercicio_01/Ejerciocio_42/Operaciones.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejerciocio_42
+{
+    class Operaciones
+    {
+        public Operaciones(int num1,int num2)
+        {
+            try
+            {
+                division(num1, num2);
+            }
+            catch (DivideByZeroException e)
+            {
+                throw e;
+            }
+        }
+
+
+        public static int division(int numeroUno, int numeroDos)
+        {
+            int resultado = 0;
+            try
+            {
+                 resultado = numeroUno / numeroDos;
+            }
+            catch (DivideByZeroException e)
+            {
+                throw e;
+            }
+            return resultado;
+        }
+    }
+}
---
+++ Ejercicio_01/Ejerciocio_42/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejerciocio_42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operaciones prueba =  new Operaciones(10,0);
+            Console.ReadLine();
+        }
+    }
+}
