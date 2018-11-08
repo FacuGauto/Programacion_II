d9b22e06253fcc2b5ce36ef729772c5c8b98d541
alumno <alumno>
2018-11-08 19:12:19 -0300

    base

--- Ejercicio_60_db/Ejercicio_60_db/Form1.Designer.cs
+++ Ejercicio_60_db/Ejercicio_60_db/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cmbProduct = new System.Windows.Forms.ComboBox();
+            this.btnCarga = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbProduct
+            // 
+            this.cmbProduct.FormattingEnabled = true;
+            this.cmbProduct.Location = new System.Drawing.Point(370, 91);
+            this.cmbProduct.Name = "cmbProduct";
+            this.cmbProduct.Size = new System.Drawing.Size(121, 21);
+            this.cmbProduct.TabIndex = 0;
+            this.cmbProduct.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnCarga
+            // 
+            this.btnCarga.Location = new System.Drawing.Point(118, 89);
+            this.btnCarga.Name = "btnCarga";
+            this.btnCarga.Size = new System.Drawing.Size(90, 23);
+            this.btnCarga.TabIndex = 1;
+            this.btnCarga.Text = "Cargar combo";
+            this.btnCarga.UseVisualStyleBackColor = true;
+            this.btnCarga.Click += new System.EventHandler(this.btnCarga_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(598, 231);
+            this.Controls.Add(this.btnCarga);
+            this.Controls.Add(this.cmbProduct);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cmbProduct;
+        private System.Windows.Forms.Button btnCarga;
     }
 }
 
--- Ejercicio_60_db/Ejercicio_60_db/Form1.cs
+++ Ejercicio_60_db/Ejercicio_60_db/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Ejercicio_60_db
 {
@@ -15,6 +16,49 @@ namespace Ejercicio_60_db
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCarga_Click(object sender, EventArgs e)
+        {
+            //Connection conexion = new Connection("SELECT name FROM Production.Product");
+            string connectionStr = "Data Source=LAB3PC24\\SQLEXPRESS;Initial Catalog =AdventureWorks2012; Integrated Security = True";
+            SqlConnection conexion = new SqlConnection(connectionStr);
+            SqlCommand comando;
+            comando = new SqlCommand();
+            comando.CommandType = System.Data.CommandType.Text;
+
+            comando.Connection = conexion;
+
+            comando.CommandText = "SELECT name FROM Production.Product";
+
+
+            try
+            {
+                //conexion.abrir();
+                conexion.Open();
+                MessageBox.Show("Se abrió la conexión con el servidor SQL Server y se seleccionó la base de datos");
+                SqlDataReader oDr = comando.ExecuteReader();
+
+                while (oDr.Read())
+                {
+                    string aux = oDr["name"].ToString();
+                    cmbProduct.Items.Add(aux);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                conexion.Close();
+                MessageBox.Show("Se cerró la conexión.");
+            }
         }
     }
 }
