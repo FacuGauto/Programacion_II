1d08942df2250e0760c36ea783553d58111b289a
alumno <alumno>
2018-11-22 20:19:31 -0300

    2 parcial

---
+++ Segparcial/c/20181122-SP/Alumno/Archivos/Sql.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Data;
+using Entidades;
+
+namespace Archivos
+{
+    public class Sql
+    {
+        private SqlCommand comando;
+        private SqlConnection conexion;
+
+        public Sql()
+        {
+
+        }
+
+        void Guardar(string tabla, Queue<Patente> datos)
+        {
+
+        }/*
+        void Leer(string tabla, out Queue<Patente> datos)
+        {
+
+        }
+        */
+    }
+}
---
+++ Segparcial/c/20181122-SP/Alumno/Archivos/Texto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Entidades;
+
+namespace Archivos
+{
+    public class Texto
+    {
+        public void Guardar(string archivo, Queue<Patente> datos)
+        {
+
+        }
+        public void Leer(string archivo, out Queue<Patente> datos)
+        { }
+    }
+}
